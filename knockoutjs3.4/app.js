08c549ae6fd67fb6e61d1478b13b5d94a1f7319c
wikin <wiki@atlas.cz>
2016-06-28 10:36:08 +0200

    knockout 3.4

---
+++ knockoutjs3.4/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function KoApp() {
+    var self = this;
+    self.items = ko.observableArray([]);
+    self.fill = function (n, callback) {
+        var i,
+        items = [];
+        for (i = 0; i < n; i += 1) {
+            items.push({
+                val: ko.observable(i)
+            });
+        }
+        self.items(items);
+        callback();
+    };
+    self.update = function (n, callback) {
+        var i;
+        ko.utils.arrayForEach(self.items(), function (item) {
+            item.val(item.val() + ' ' + item.val());
+        });
+        callback();
+    };
+    self.clear = function (callback) {
+        self.items([]);
+        callback();
+    };
+
+    self.insert = function (n, callback) {
+        self.items.splice(n, 0, {
+            val: ko.observable('xx')
+        })
+   
+        callback();
+    };
+}
+
+
+
+ko.components.register('list', {
+    template: '<ul data-bind="foreach: items"><!-- ko component: {name:\'item\', params:$data } --><!-- /ko --></ul>'
+});
+
+ko.components.register('item', {
+    template: '<li>jj: <span data-bind="text: val"></span></li>'
+});
+
+
+var koApp = new KoApp();
+$(function() {
+  ko.applyBindings(koApp, $('body').get(0));
+});
+
+ENV.append({
+    code: 'knockoutjs34',
+    clear: koApp.clear,
+    fill: koApp.fill,
+    update: koApp.update,
+    insert: koApp.insert
+
+})
